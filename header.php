897fcfae1080d08a9d9718d23a286d31dac15d70
Herald Yu <yuhr123@gmail.com>
2017-07-06 13:58:26 +0800

    header 增加网站描述 meta 标签

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="GetNAS 是技术指南创作与分享平台，专注于创作高质量的原创技术教程，内容涉及 FreeNAS、Ubuntu、云计算、NextCloud、Syncthing、Resilio Sync 以及树莓派等。">
     <?php if ( is_home() ): ?>
     <title><?php bloginfo('name') ; echo ' - ' ; bloginfo('description'); ?></title>
     <?php elseif ( is_single() || is_page() ): ?>
@@ -213,7 +214,7 @@
                             </ul>
                         </div>
                     <?php endif;?>
-                    
+
                     <?php if ( is_category('resilio-sync') ) : ?>
                         <div class="relate-link">
                             <ul>
